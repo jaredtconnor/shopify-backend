6054f0559b475656ef81f7703045be214efefc4e
Jared Connor <jaredconnor301@gmail.com>
2022-05-11 20:00:03 -0700

    Working product routes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from urllib import response
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+
+from models import Products, product_pydantic, product_pydanticIn
 
 app = FastAPI()
 
@@ -7,6 +10,54 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"Hello World"}
+
+
+@app.post("/product")
+async def add_product(product_info: product_pydantic):
+    product_obj = await Products.create(**product_info.dict(exclude_unset=True))
+    response = await product_pydantic.from_tortoise_orm(product_obj)
+    return {"status": "ok", "data": response}
+
+
+@app.get("/product")
+async def get_products():
+
+    response = await product_pydantic.from_queryset(Products.all())
+    return {"status": "ok", "data": response}
+
+
+@app.get("/product/{product_id}")
+async def get_product(product_id: int):
+
+    response = await product_pydantic.from_queryset_single(Products.get(id=product_id))
+    return {"status": "ok", "data": response}
+
+
+@app.put("/product/{product_id}")
+async def update_product(product_id: int, update_info: product_pydantic):
+
+    product = await Products.get(id=product_id)
+
+    updated_product = update_info.dict(exclude_unset=True)
+
+    product.name = update_info["name"]
+    product.category = update_info["category"]
+    product.inventory = update_info["inventory"]
+    product.price = update_info["price"]
+
+    await Products.save()
+
+    respone = await product_pydantic.from_tortoise_orm(product)
+    return {"status": "ok", "data": response}
+
+
+@app.delete("/product/{product_id}") 
+async def delete_product(product_id: int): 
+
+    response = await Products.filter(id = product_id).delete()
+    return {"status": "ok", "data": response}
+
+
 
 
 register_tortoise(
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from tortoise.models import Model
 from tortoise import fields
-
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 class Products(Model):
 
     id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=56)
     category = fields.CharField(max_length=56, null=False)
     inventory = fields.IntField(default=0)
     price = fields.DecimalField(max_digits=8, decimal_places=2, default=0.000)
@@ -14,6 +15,8 @@ class Purchasers(Model):
 
     id = fields.IntField(pk=True)
     number_items = fields.IntField(default=0)
+    product = fields.ForeignKeyField("models.Products")
+    supplier = fields.ForeignKeyField("models.Suppliers")
 
 
 class Suppliers(Model):
@@ -27,4 +30,12 @@ class Orders(Model):
     id = fields.IntField(pk=True)
     order_date = fields.DateField(auto_now_add=True)
     customer_name = fields.CharField(max_length=56, null=False)
-    number_items = fields.IntField(default=0)
+    number_items = fields.IntField(default=0) 
+    product = fields.ForeignKeyField("models.Products")
+
+
+product_pydantic = pydantic_model_creator(Products, name="Products")
+product_pydanticIn = pydantic_model_creator(Products, name="ProductIn", exclude_readonly=True) 
+
+supplier_pydantic = pydantic_model_creator(Suppliers, name="Suppliers")
+supplier_pydanticIn = pydantic_model_creator(Suppliers, name="SuppliersIn", exclude_readonly=True)
