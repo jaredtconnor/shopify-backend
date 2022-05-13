4312ef99e9ea3837f26e83c288278521b86d52ad
Jared Connor <jaredconnor301@gmail.com>
2022-05-12 20:51:20 -0700

    Brokend product table structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,17 @@ import react from "react"
 import { Link, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
+import { ProductContext } from "./ProductContext";
+import ProductsTable from "./components/ProductsTable"
 
 function App() {
   return (
 
     <div>
-        <NavBar />
+      <NavBar />
+      <ProductContext.Provider>
+        <ProductsTable />
+      </ProductContext.Provider>
     </div>
 
   );
