44acfe9c351fb9c39c4b894301e5c1cbe121fce9
Jared Connor <jaredconnor301@gmail.com>
2022-05-19 08:49:24 -0700

    Adding config files and making db public

---
+++ build.sh
@@ -0,0 +1,30 @@
+echo "Checking Python version..."
+python3.9 --version
+retVal=$?
+if [ $retVal -ne 0 ]
+then
+    echo "Running initial setup..." 
+
+    echo "Installing Python 3.9..."
+    install-pkg python3.9
+
+    echo
+    echo "Installing Python dependencies..."
+    python -m venv env
+    . env/bin/activate
+    pip install -r requirements.txt
+
+    echo
+    echo "Installing JavaScript dependencies..."
+    cd frontend && npm install
+    cd ../
+
+    echo
+    retVal=$?
+    if [ $retVal -ne 0 ]
+    then
+      exit 1
+    fi
+fi
+
+echo
